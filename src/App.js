116288d1f04f26bc1895c29a01058cf3de22be7c
jor-g-r <jfrrj19@gmail.com>
2023-08-03 17:05:26 -0500

    Tailwind added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 const App = () => {
   return (
     <div className="app">
-     
+      <h1 class="text-3xl font-bold underline text-purple-500">
+    Hello world!
+  </h1>
     </div>
   );
 }
