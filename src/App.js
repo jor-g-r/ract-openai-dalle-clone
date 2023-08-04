2f820620efa76104aaef3ed85e2367a78798abf3
jor-g-r <jfrrj19@gmail.com>
2023-08-04 13:09:07 -0500

    starting generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 
 const App = () => {
+  const surpriseOpt = [
+    'a space monkey', 'a sea bat'
+  ];
   return (
     <div className="app">
-      <h1 class="text-3xl font-bold underline text-purple-500">
-    Hello world!
-  </h1>
+      <section className="search-section">
+          <div className="container">
+            <div className="">
+              <p>Start with a detailed description <span className="text-green-800">Surprise me</span></p>
+              <div className="input-container">
+                <input type="text" placeholder="a multicolor butterfly..." /> 
+                <button>Generate</button>
+              </div>
+            </div>
+          </div>
+      </section>
+
+      <section className="image-results">
+
+      </section>
     </div>
   );
 }
