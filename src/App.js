05fa38ad1987274ff3091fde150e557dedbd0130
jor-g-r <jfrrj19@gmail.com>
2023-08-05 20:24:13 -0500

    slightly styled

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ const App = () => {
   return (
     <div className="app">
       <section className="search-section">
-          <div className="container">
-            <div className="">
+          <div className="container mx-auto">
+            <div className="px-8 flex flex-col items-center pt-12">
               <p>Start with a detailed description <span className="text-green-800">Surprise me</span></p>
-              <div className="input-container">
-                <input type="text" placeholder="a multicolor butterfly..." /> 
-                <button>Generate</button>
+              <div className="input-container py-3">
+                <input className="w-96 h-10 p-2 rounded-md mr-2 border focus-visible:border-gray-500 focus-visible:border-none" type="text" placeholder="a multicolor butterfly..." /> 
+                <button className="generate-btn uppercase text-xs">Generate</button>
               </div>
             </div>
           </div>
