67bfb081e0b8b806a82a82abe7699c7899f3bca0
jor-g-r <jfrrj19@gmail.com>
2023-08-06 13:38:39 -0500

    server starting settings

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const PORT = 8000 ;
 const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
+
+app.listen(PORT, () => console.log('Server running on PORT ' + PORT) )
