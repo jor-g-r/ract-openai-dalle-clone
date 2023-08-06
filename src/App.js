844debb578d766d9a5deb7381d3689b2b3c83f58
jor-g-r <jfrrj19@gmail.com>
2023-08-06 12:52:06 -0500

    a deeper styled

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => {
             <div className="px-8 flex flex-col items-center pt-12">
               <p>Start with a detailed description <span className="text-green-800">Surprise me</span></p>
               <div className="input-container py-3">
-                <input className="w-96 h-10 p-2 rounded-md mr-2 border focus-visible:border-gray-500 focus-visible:border-none" type="text" placeholder="a multicolor butterfly..." /> 
-                <button className="generate-btn uppercase text-xs">Generate</button>
+                <input className="w-96 h-10 py-2 px-3 rounded-xl mr-2 border focus-visible:outline-none focus-visible:border-green-600 focus-visible:border-opacity-70 outline-1 placeholder:font-light placeholder:tracking-wide placeholder:text-sm bg-gray-50" type="text" placeholder="A multicolor butterfly..." /> 
+                <button className="generate-btn uppercase text-xs tracking-wide text-green-950">Generate</button>
               </div>
             </div>
           </div>
