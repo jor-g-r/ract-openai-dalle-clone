cda0cefd5bd1a5a5521da42c5231a7be203c7c22
jor-g-r <jfrrj19@gmail.com>
2023-08-08 19:44:24 -0500

    POST logic from server.js and App.js working fine

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-const PORT = 8000 ;
+
+
+const PORT = 8000 
 const express = require('express')
 const cors = require('cors')
-
+const { Configuration, OpenAIApi } = require("openai")
 const app = express()
 app.use(cors())
 app.use(express.json())
-require('dotenv').config()
 
-const { Configuration, OpenAIApi } = require("openai")
+require('dotenv').config()
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.API_KEY,
 });
 
 const openai = new OpenAIApi(configuration)
 
 
-app.post('/images', async(req, resp) =>{
+//route
+app.post('/images', async(req, res) =>{
     
     try {
         const response = await openai.createImage({
-            prompt: "A cute baby sea otter",
+            prompt: "A multicolor butterfly",
             n: 2,
             size: "1024x1024",
         })
-        resp.send(response.data.data)
-        console.log(response)
+        console.log(response.data.data)
+        res.send(response.data.data)
+        
     } catch (error) {
         console.error(error)
     }
 
-})
+});
 
 
-app.listen(PORT, () => console.log('Server running on PORT ' + PORT) )
+app.listen(PORT, () => console.log('Server running on PORT ' + PORT))
--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
         body: JSON.stringify({
             message : "Plop" 
           }),
+        mode : 'cors',
         headers: {
           "Content-type" : "application/json"
         } 
@@ -32,8 +33,13 @@ const App = () => {
             <div className="px-8 flex flex-col items-center pt-12">
               <p>Start with a detailed description <span className="text-green-800">Surprise me</span></p>
               <div className="input-container py-3">
-                <input className="w-96 h-10 py-2 px-3 rounded-2xl mr-2 border focus-visible:outline-none focus-visible:border-green-600 focus-visible:border-opacity-70 outline-1 placeholder:font-light placeholder:tracking-wide placeholder:text-sm bg-gray-50" type="text" placeholder="A multicolor butterfly..." /> 
-                <button className="generate-btn uppercase text-xs tracking-wide text-green-950">Generate</button>
+                
+                <input className="w-96 h-10 py-2 px-3 rounded-2xl mr-2 border focus-visible:outline-none focus-visible:border-green-600 focus-visible:border-opacity-70 outline-1 placeholder:font-light placeholder:tracking-wide placeholder:text-sm bg-gray-50" 
+                       type="text" 
+                       placeholder="A multicolor butterfly..." 
+                />
+
+                <button className="generate-btn uppercase text-xs tracking-wide text-green-950" onClick={getImages} >Generate</button>
               </div>
             </div>
           </div>
